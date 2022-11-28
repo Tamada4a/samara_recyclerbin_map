c1c834cec32e0f024f9f729a4c7541a7cc8a22ae
ugly4 <Fedyakin.A.E@yandex.ru>
2022-11-29 02:03:28 +0400

    Добавление определения местоположения

--- app/src/main/java/com/example/samara_recyclerbin_map/Main.java
+++ app/src/main/java/com/example/samara_recyclerbin_map/Main.java
@@ -1,30 +1,64 @@
 package com.example.samara_recyclerbin_map;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.IconStyle;
 import com.yandex.mapkit.map.MapObjectCollection;
 import com.yandex.mapkit.map.PlacemarkMapObject;
+import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.map.internal.PlacemarkMapObjectBinding;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
 import com.yandex.runtime.image.ImageProvider;
 
 
-public class Main extends AppCompatActivity {
+public class Main extends Activity implements UserLocationObjectListener {
 
     private MapView mapview;
     private final Point START_POINT = new Point(53.212228298365396, 50.17742481807416);
     private final Point TEST = new Point(53.212857,50.182195);
+    private UserLocationLayer userLocationLayer;
+
+
+    public static class recyclingPoint{
+
+        float latitude;
+        float longitude;
+        String location;
+        String type;
+        String info;
+
+        public recyclingPoint(float latitude, float longitude, String location, String type, String info){
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.location = location;
+            this.type = type;
+            this.info = info;
+        }
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +68,27 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mapview = (MapView)findViewById(R.id.mapview);
+        mapview = findViewById(R.id.mapview);
+        mapview.getMap().setRotateGesturesEnabled(true);
         mapview.getMap().move(
-                new CameraPosition(START_POINT, 15.0f, 0.0f, 0.0f),
+                new CameraPosition(START_POINT, 1.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 3),
                 null);
-        MapObjectCollection mapObjects = mapview.getMap().getMapObjects().addCollection();
-        //PlacemarkMapObject marker = mapObjects.addPlacemark(START_POINT);
-        PlacemarkMapObject markerHome = mapObjects.addPlacemark(START_POINT, ImageProvider.fromBitmap(drawSimpleBitmap("1")));
-        PlacemarkMapObject markerTest = mapObjects.addPlacemark(TEST, ImageProvider.fromBitmap(drawSimpleBitmap("1")));
 
-        //Bitmap source = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_foreground);
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapview.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+
+        MapObjectCollection mapObjects = mapview.getMap().getMapObjects().addCollection();
+
+        PlacemarkMapObject markerHome = mapObjects.addPlacemark(START_POINT, ImageProvider.fromBitmap(drawSimpleBitmap("HOME", "green")));
+        PlacemarkMapObject markerTest = mapObjects.addPlacemark(TEST, ImageProvider.fromBitmap(drawSimpleBitmap("DNS", "blue")));
+
+        //Bitmap testt = BitmapFactory.decodeFile("maps.bmp");
+        //PlacemarkMapObject markerTest = mapObjects.addPlacemark(TEST, ImageProvider.fromBitmap(testt));
+
         //marker.setIcon(ImageProvider.fromBitmap(source));
         //marker.setIcon(ImageProvider.fromResource(this,R.drawable.ic_launcher_foreground));
     }
@@ -63,22 +107,69 @@ public class Main extends AppCompatActivity {
         mapview.onStart();
     }
 
-    public Bitmap drawSimpleBitmap(String number) {
+    public Bitmap drawSimpleBitmap(String text, String colorr) {
         int picSize = 80;
         Bitmap bitmap = Bitmap.createBitmap(picSize, picSize, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         // отрисовка плейсмарка
         Paint paint = new Paint();
-        paint.setColor(getResources().getColor(R.color.purple_200));
+        if (colorr == "green")
+            paint.setColor(getResources().getColor(R.color.green));
+        else
+            paint.setColor(getResources().getColor(R.color.purple_700));
         paint.setStyle(Paint.Style.FILL);
         canvas.drawCircle(picSize / 2, picSize / 2, picSize / 2, paint);
         // отрисовка текста
         paint.setColor(Color.WHITE);
         paint.setAntiAlias(true);
-        paint.setTextSize(1);
+        paint.setTextSize(30);
         paint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawText(number, picSize / 2,
+        canvas.drawText(text, picSize / 2,
                 picSize / 2 - ((paint.descent() + paint.ascent()) / 2), paint);
         return bitmap;
     }
+
+    @Override
+    public void onObjectAdded(@NonNull UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.5)),
+                new PointF((float)(mapview.getWidth() * 0.5), (float)(mapview.getHeight() * 0.83)));
+
+        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
+                this, R.drawable.user_arrow));
+
+        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
+
+        pinIcon.setIcon(
+                "icon",
+                ImageProvider.fromResource(this, R.drawable.icon),
+                new IconStyle().setAnchor(new PointF(0f, 0f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(0f)
+                        .setScale(1f)
+        );
+
+        pinIcon.setIcon(
+                "pin",
+                ImageProvider.fromResource(this, R.drawable.search_result),
+                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
+                        .setRotationType(RotationType.ROTATE)
+                        .setZIndex(1f)
+                        .setScale(0.5f)
+        );
+
+        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
+    }
+
+    @Override
+    public void onObjectRemoved(@NonNull UserLocationView userLocationView) {
+
+    }
+
+    @Override
+    public void onObjectUpdated(@NonNull UserLocationView userLocationView, @NonNull ObjectEvent objectEvent) {
+
+    }
+
+
 }
