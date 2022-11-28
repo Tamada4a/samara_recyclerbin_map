a78a6c88c078481c0bfe614c040b48e9f6c9eaff
ugly4 <Fedyakin.A.E@yandex.ru>
2022-11-28 23:38:17 +0400

    add markers

--- app/src/main/java/com/example/samara_recyclerbin_map/Main.java
+++ app/src/main/java/com/example/samara_recyclerbin_map/Main.java
@@ -1,5 +1,10 @@
 package com.example.samara_recyclerbin_map;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,7 @@ public class Main extends AppCompatActivity {
 
     private MapView mapview;
     private final Point START_POINT = new Point(53.212228298365396, 50.17742481807416);
+    private final Point TEST = new Point(53.212857,50.182195);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +36,17 @@ public class Main extends AppCompatActivity {
 
         mapview = (MapView)findViewById(R.id.mapview);
         mapview.getMap().move(
-                new CameraPosition(START_POINT, 11.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
+                new CameraPosition(START_POINT, 15.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 3),
                 null);
-        /*MapObjectCollection mapObjects = mapview.getMap().getMapObjects().addCollection();
-        PlacemarkMapObject marker = mapObjects.addPlacemark(START_POINT);
-        marker.setIcon(ImageProvider.fromResource(this,R.drawable));*/
+        MapObjectCollection mapObjects = mapview.getMap().getMapObjects().addCollection();
+        //PlacemarkMapObject marker = mapObjects.addPlacemark(START_POINT);
+        PlacemarkMapObject markerHome = mapObjects.addPlacemark(START_POINT, ImageProvider.fromBitmap(drawSimpleBitmap("1")));
+        PlacemarkMapObject markerTest = mapObjects.addPlacemark(TEST, ImageProvider.fromBitmap(drawSimpleBitmap("1")));
+
+        //Bitmap source = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_foreground);
+        //marker.setIcon(ImageProvider.fromBitmap(source));
+        //marker.setIcon(ImageProvider.fromResource(this,R.drawable.ic_launcher_foreground));
     }
 
     @Override
@@ -50,5 +61,24 @@ public class Main extends AppCompatActivity {
         super.onStart();
         MapKitFactory.getInstance().onStart();
         mapview.onStart();
+    }
+
+    public Bitmap drawSimpleBitmap(String number) {
+        int picSize = 80;
+        Bitmap bitmap = Bitmap.createBitmap(picSize, picSize, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        // отрисовка плейсмарка
+        Paint paint = new Paint();
+        paint.setColor(getResources().getColor(R.color.purple_200));
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(picSize / 2, picSize / 2, picSize / 2, paint);
+        // отрисовка текста
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+        paint.setTextSize(1);
+        paint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(number, picSize / 2,
+                picSize / 2 - ((paint.descent() + paint.ascent()) / 2), paint);
+        return bitmap;
     }
 }
